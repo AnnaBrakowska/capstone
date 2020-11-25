c91dc2879f75b0dab9aee79300d2bf2144a55665
Anna <anna.brakowska91@gmail.com>
2020-11-25 12:23:22 -0500

    finished config

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_81VlxnwlR',
         userPoolClientId: '7j4qi4p3m5oiai3bbedranqc41',
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: ''
+        invokeUrl: 'https://coivjisozl.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
